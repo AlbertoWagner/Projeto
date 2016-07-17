ad163c74d0877a7928228f133f8bb4fdf00181d5
Victor Araujo <victor.oliveira@ccc.ufcg.edu.br>
2016-07-17 16:54:44 -0300

    add setEmail e inicializacao de Arquivo

--- Usuario.java
+++ Usuario.java
@@ -1,19 +1,21 @@
 package models;
 
 import java.util.*;
+//Classe usuario onde são definidas os parametros de usuario, senha e email
 
 public class Usuario {
 	private String username;
 	private String senha;
 	private String email;
+	Arquivo arquivo = new Arquivo();
 	Diretorio diretorioPrincipal = new Diretorio();
-	
+	// Construtor
 	public Usuario(String username, String senha, String email){
 		this.username = username;
 		this.senha = senha;
 		this.email = email;
 	}
-	
+	// Getters & Setters - Start
 	public String getUsername() {
 		return username;
 	}
@@ -30,8 +32,8 @@ public class Usuario {
 		return email;
 	}
 	
-	
-	
-	
-	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	// Getters & Setters - End
 }
